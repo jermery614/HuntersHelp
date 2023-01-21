551ab1892064ddad6416df395b1dfd127fb2b6ce
Jermery Wheeler <Jermery Wheeler>
2023-01-20 21:05:01 -0500

    Rolling Back

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,76 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-
-
-// const ps = require("prompt-sync");
-// const prompt = ps();
-// let usrAge = prompt("What is your age ?");
-
-router.use('/calibre', require('./calibre'));
-router.use('/', require('./swagger'));
-
-// function userPerson(person) {
-//     try{
-//          if (person?.age == null){
-//         console.log("Sorry, you must enter an age.")
-//         return
-//     }
-//     if (person.age >= 18){
-//         console.log("Welcome")
+// const prm = require('prompt');
+// const prompt = require('prompt-sync')();
+// const name = prompt('Hello, what is your name? ');
+// const age = prompt('What is your age? ');
+// try {
+//     if (age == null) {
+//         console.log("please enter your age")
+//         return age
+//     } if ( name == null) {
+//         console.log("please enter a name")
+//         return name
+//     } if (age <= 17) { 
+//         return age.status(501).json({error: " Sorry," +name+" you are not old enough."})
+        
+//     } else {
+//         console.log("Welcome, "+ name);
 //         router.use('/calibre', require('./calibre'));
 //         router.use('/', require('./swagger'));
 //     }
-//     else {
-//         console.log("Your are too young")
-//         process.exit()
-//     }
+// }catch (err) {
+//     res.status(500).json(err);
+// };
+
+
+// prm.start()
+// prm.get(['username', 'userage'], function (err, result) {
+//     if (err) { console.log('got an error: ', err) }
+//     console.log( 'Command-line input received: ')
+//     console.log('  Username: ' + result.username)
+//     console.log('  Userage: ' + result.userage)
+//     try {
+//         if ( result.userage <= 17)  {return result.status(501).json({error: 'User is not old enough, yet. '})
+
+//         } else {
+//             console.log("Welcome")
+//             router.use('/calibre', require('./calibre'));
+//             router.use('/', require('./swagger'));
+//         }
 //     } catch (err) {
-//         res.status(500).json(err);
-//     }
+//                 res.status(500).json(err);
+//             }
+
+// });
+
+
+
+function userPerson(person) {
+    try{
+         if (person?.age == null){
+        console.log("Sorry, you must enter an age.")
+        return
+    }
+    if (person.age >= 18){
+        console.log("Welcome")
+        router.use('/calibre', require('./calibre'));
+        router.use('/', require('./swagger'));
+    }
+    else {
+        console.log("Your are too young")
+        process.exit()
+    }
+    } catch (err) {
+        res.status(500).json(err);
+    }
    
 
-// };
-// const p = {
-//     age:22,
-// }
-// userPerson(p)
+};
+const p = {
+    age:12,
+}
+userPerson(p)
 
 
 // const p1 = {
