0dcf91c5e4c37028b19ae3d2c12555ad6ecaa861
Jermery Wheeler <Jermery Wheeler>
2023-01-22 00:50:30 -0500

    adding validation to all endpoints

--- routes/calibre.js
+++ routes/calibre.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const calibreController = require('../controllers/calibre');
 const {ageValidation}= require('../utils/validation');
 
-router.get('/', calibreController.getAll);
-router.get('/:id', calibreController.getSingle);
+router.get('/',ageValidation, calibreController.getAll);
+router.get('/:id', ageValidation, calibreController.getSingle);
 router.post('/', ageValidation ,calibreController.createCalibre);
 router.put('/:id',ageValidation, calibreController.updateCalibre);
-router.delete('/:id', calibreController.deleteCalibre);
+router.delete('/:id',ageValidation, calibreController.deleteCalibre);
 
 module.exports = router;
