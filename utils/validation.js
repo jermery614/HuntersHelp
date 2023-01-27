d8e95a89e68ac8b1f303903d1e8f2335f14a947f
Jermery Wheeler <Jermery Wheeler>
2023-01-26 23:06:10 -0500

    updating Auth0 setting.

--- utils/validation.js
+++ utils/validation.js
@@ -1,4 +1,4 @@
-const { default: axios } = require('axios');
+// const { default: axios } = require('axios');
 const { check } = require('express-validator');
 // const {tokenCheck} = require('axios');
 // const { tokenCheck} = require('express-validator');
