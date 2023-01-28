a7f94595e0cc4d7e553be785c80de6db1cf6d146
Jermery Wheeler <Jermery Wheeler>
2023-01-28 05:42:10 -0500

    Testing Auth0 Login possible fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/calibre', require('./calibre'));
-router.use('/users', require('./users'));
 router.use('/', require('./swagger'));
 
 
