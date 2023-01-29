eb82658c5948c7247acd952a54004717fa10c880
Jermery Wheeler <Jermery Wheeler>
2023-01-28 21:58:29 -0500

    adding admin validation to the users routes/validation.js

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router();
 const usersController = require('../controllers/users');
+const { isAdmin } = require('../utils/validation');
 // const { ageValidation } = require('../utils/validation');
 
 //normal functions
-router.get('/', usersController.getAllUsers);
-router.get('/:id', usersController.getSingleUSer);
-router.post('/', usersController.createUser);
-router.put('/:id', usersController.updateUsers);
-router.delete('/:id', usersController.deleteUsers);
+router.get('/',isAdmin, usersController.getAllUsers);
+router.get('/:id',isAdmin, usersController.getSingleUSer);
+router.post('/',isAdmin, usersController.createUser);
+router.put('/:id',isAdmin, usersController.updateUsers);
+router.delete('/:id',isAdmin, usersController.deleteUsers);
 
 // Testing Oauth
 
